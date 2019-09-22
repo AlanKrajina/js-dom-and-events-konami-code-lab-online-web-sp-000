b469e28f663fed614a4ce67ef5a0a1cb144bf598
Alan Krajina <alankrajina88@gmail.com>
2019-09-22 09:32:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,8 +8,9 @@ function init() {
 
 
     const key = parseInt(e.detail || e.which);
+    var requiredKey = code[index];
 
-    if (key === code[index]) {
+    if (key === requiredKey) {
       index++;
 
       if (index === code.length) {
