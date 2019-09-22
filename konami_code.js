ffdf8e55842ea596e2b43be3427752c523471e41
Alan Krajina <alankrajina88@gmail.com>
2019-09-22 09:07:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  let index = 0;
 
-  let key = document.getElementsByTagName("BODY")
-  key.addEventListener('keydown', function(e){
+  let bod = document.getElementsByTagName("BODY")
+  bod.addEventListener('keydown', function(e){
+
+
+    const key = parseInt(e.detail || e.which);
+   
+    if (key === alphabet[index]) {
+      index++;
+   
+      if (index === alphabet.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+
+
 
   }
 
