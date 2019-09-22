0cdd4eb5d2305596a2eb2177cbc33e9d286f62f2
Alan Krajina <alankrajina88@gmail.com>
2019-09-22 09:12:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
 
 function init() {
+  let index = 0;
 
   let bod = document.getElementsByTagName("BODY")
   bod.addEventListener('keydown', function(e){
